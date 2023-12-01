0ee8061ad6781e1d6d7fe1628f790696a92036a7
Raul Elgueta <rielgueta@uc.cl>
2023-11-30 21:35:30 -0300

    Weighted randomness implemented

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,16 +1,18 @@
 extends Node2D
 var grid_size = 16
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Sprite2D.show()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# comentadas por si las utilizamos en el futuro
+#func _ready():
+#	$Sprite2D.show()
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
 func _unhandled_key_input(event):
+	# Cuando se presiona una tecla, revisa la dirección en la cuál se debería mover y llama a la funcón mover
+	# Es cuando se suelta la tecla para que solo se mueva una celda
 	var direction = Vector2.ZERO
 	if event.is_action_released("ui_up"):
 		direction.y = -1
@@ -23,8 +25,9 @@ func _unhandled_key_input(event):
 	move(direction)
 
 func move(dir):
-	# TODO reemplazar condición por un raycast
-	$RayCast2D.target_position = dir*16
+	# Lanza un "rayo laser" en la dirección en la que se quiere mover y si no colisiona con nada...
+	$RayCast2D.target_position = dir*grid_size
 	$RayCast2D.force_raycast_update()
 	if not $RayCast2D.is_colliding():
+		# Entonces se mueve una celda (El tamaño de la celda)
 		position += dir*grid_size
--- Scripts/tilemap.gd
+++ Scripts/tilemap.gd
@@ -1,7 +1,6 @@
 extends TileMap
 
-# Un diccionario con las coordenadas de las tiles correspondientes
-
+# Variable del ancho del laberinto
 @export var ancho_laberinto = 8
 var arriba = 0
 var izquierda = 1
@@ -9,38 +8,51 @@ var abajo = 2
 var derecha = 3
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(0, 8 , 1):
-		for j in range(0, 8, 1):
+	for i in range(0, ancho_laberinto, 1):
+		for j in range(0, ancho_laberinto, 1):
 			var posicion = asignar_movimientos(i, j)
 			set_cell(0, Vector2i(i, j), 0, posicion)
-			
+
 	# agua(0 , 0)
-	for i in range(0, 8 , 1):
-		for j in range(0, 8, 1):
+	for i in range(0, ancho_laberinto, 1):
+		for j in range(0, ancho_laberinto, 1):
 			# if not (x,y).isagua():
 				# arreglar()
 			pass
 
 func asignar_movimientos(x, y):
+	# Conexiones con las celdas cercanas
 	var movimientos = [false, false, false, false]
 
+	# Si no es la primera fila
 	if x != 0:
+		# Revisa si la celda de la izquierda está conectada con la actual, si es así, declara que está conectada
 		var cell_izq = get_cell_atlas_coords(0, Vector2i(x - 1, y))
-		if get_moves(cell_izq)[derecha]:
+		if get_moves(cell_izq,derecha):
 			movimientos[izquierda] = true
 
+	# Lo mismo pero para las columnas
 	if y != 0:
 		var cell_arr = get_cell_atlas_coords(0, Vector2i(x, y - 1))
-		if get_moves(cell_arr)[abajo]:
+		if get_moves(cell_arr, abajo):
 			movimientos[arriba] = true
 
-		# Para eso contamos la cantidad de conexiones que queremos que tenga
-		# random.choices se le entrega una lista de posibles resultados y una lista de la probabildad de cada uno
-	# var cantidad_de_conexiones = random.choices([1, 2, 3, 4], [0.2, 0.5, 0.25, 0.05])
-	var cantidad_de_conexiones = randi_range(1, 4)
+	# Para eso contamos la cantidad de conexiones que queremos que tenga
+	# 3/20 1 conexion
+	# 10/20 2 conexiones
+	# 7/20 3 conexiones
+	# 1/20 4 conexiones
+	var n_aleatorio = randi_range(0, 19)
+	var cantidad_de_conexiones = 1
+	if 4 <= n_aleatorio:
+		cantidad_de_conexiones += 1
+	if 13 <= n_aleatorio:
+		cantidad_de_conexiones += 1
+	if 19 == n_aleatorio:
+		cantidad_de_conexiones += 1
 
-		# Posteriormente contamos la cantidad actual de conexiones que se tienen, considerando las que estaban
-		# determinadas por casillas anteriores
+	# Posteriormente contamos la cantidad actual de conexiones que se tienen, considerando las que estaban
+	# determinadas por casillas anteriores
 	var cantidad_actual = 0
 	for i in range(0, 4, 1):
 		if movimientos[i]:
@@ -55,7 +67,6 @@ func asignar_movimientos(x, y):
 
 		# Si todavía queda posibilidad (Sea porque habían 4 caminos) se agregará una abertura hacia el ultimo lado
 			if cantidad_actual < cantidad_de_conexiones:
-
 				# Notese que este comando (pos = 5-pos) se encarga de transformar la posición anterior a la otra disponible
 				# (Entre las opciones 2 y 3). Si anteriormente se eligió la puerta 2 -> 5 - 2 = 3. Y viceversa
 				# Si anteriormente se eligió la 3 -> 5 - 3 = 2. Así nos aseguramos que se eligirá la otra puerta disponible
@@ -71,10 +82,10 @@ func asignar_movimientos(x, y):
 		movimientos[abajo] = false
 	
 	var posicion = convert_to_cell(movimientos)
-	
 	return posicion
 
-func get_moves(cell=Vector2i.ZERO):
+func get_moves(cell: Vector2i, direccion=-1):
+	# Array que guarda las conexiones asociadas a una coordenada del Atlas
 	var movimientos = [false, false, false, false]
 	
 	if cell.y == 1 or cell.y == 3:
@@ -85,10 +96,14 @@ func get_moves(cell=Vector2i.ZERO):
 		movimientos[abajo] = true
 	if cell.x == 2 or cell.x == 3:
 		movimientos[derecha] = true
-	
-	return movimientos
+	# Si se le especifica una coordenada, solamente retorna esa
+	if direccion == -1:
+		return movimientos
+	else:
+		return movimientos[direccion]
 	
 func convert_to_cell(movimientos):
+	# Dada una lista con las conexiones posibles, retorna el ID de la celda asociada
 	var posicion = Vector2i.ZERO
 	
 	if movimientos[0]:
