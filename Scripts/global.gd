89fb2cda47ddeecf892b24e98952c7b2d4a19df2
Raul Elgueta <rielgueta@uc.cl>
2024-03-14 14:45:04 -0300

    Added Items and inventory scripts
    In globals added a list of possible objects in a dictionary(items).
    Created an inventory script for managing the items

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,3 +1,20 @@
 extends Node
 
 var score = ["00", "00", "0"]
+
+var items
+
+func _ready():
+	items = read_from_JSON("res://Scripts/items.json")
+	for key in items.keys():
+		items[key]["key"] = key
+
+func read_from_JSON(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	var json = JSON.new()
+	json.parse(file.get_as_text())
+	return json.data
+
+func get_item_by_key(key):
+	if items and items.has(key):
+		return items[key].duplicate(true)
