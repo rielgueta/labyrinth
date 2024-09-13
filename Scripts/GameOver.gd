5017d057d840f2898fbebb6633cf17469de1e16b
Raul Elgueta <rielgueta@uc.cl>
2024-09-13 16:30:30 -0300

    Ahora si esta funcionando y el menu se reescala

--- Scripts/GameOver.gd
+++ Scripts/GameOver.gd
@@ -1,7 +1,8 @@
 extends Control
 
 
-
+func _ready():
+	$BackToMenu.grab_focus()
 
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/Inicio.tscn")
--- Scripts/lose.gd
+++ Scripts/lose.gd
@@ -1,5 +1,9 @@
 extends Control
 
 
+func _ready():
+	$Button.grab_focus()
+
+
 func _on_button_button_up():
 	get_tree().change_scene_to_file("res://scenes/Inicio.tscn")
