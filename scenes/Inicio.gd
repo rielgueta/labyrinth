cb7c8dd458e966b77426ead27aa4ad48b49d290e
Raul Elgueta <rielgueta@uc.cl>
2024-09-13 14:40:29 -0300

    Ahora si, menu inicio reescalable

--- scenes/Inicio.gd
+++ scenes/Inicio.gd
@@ -1,6 +1,9 @@
-extends Control
+extends MarginContainer
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Contenido/opciones/StartButton.grab_focus()
 
 func start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
